5725139efb5314aa5530898f848b897c8ebdeec0
Block354 <75645441+Block354@users.noreply.github.com>
2021-12-23 22:46:29 +1300

    feat(invite): restructured

--- commands/Utility/invite.js
+++ commands/Utility/invite.js
@@ -15,8 +15,18 @@ module.exports = {
         const embed = new MessageEmbed()
             .setColor("BLACK")
             .setAuthor("About Me")
-            .setDescription(`A music bot for Discord. \nWith support for YouTube, Spotify and Soundcloud!\n\n[Invite the bot here](${client.config.app.invite})`)
+            .setDescription(client.config.app.slogan.split(".").join(".\n"))
             .setThumbnail(message.guild.iconURL())
+            .setFields(
+                {
+                    name: "Invite",
+                    value: "[`Click Here`](" + client.config.app.invite + ")"
+                },
+                {
+                    name: "Support Server",
+                    value: "[`Click Here`](" + client.config.app.support_server + ")"
+                }
+            )
             .setTimestamp(new Date())
             .setFooter("Thanks For Choosing Bass", client.config.app.logo);
 
