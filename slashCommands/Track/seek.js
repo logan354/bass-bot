8db16fddc2753ca5559b7e01889b1df782e6da7e
Block354 <75645441+Block354@users.noreply.github.com>
2022-01-18 19:26:16 +1300

    fix: typing errors

--- slashCommands/Track/seek.js
+++ slashCommands/Track/seek.js
@@ -21,17 +21,17 @@ module.exports = {
      * @param {CommandInteractionOptionResolver} args 
      */
     async execute(client, interaction, args) {
-        const serverQueue = client.queues.get(message.guild.id);
-        const voiceChannel = message.member.voice.channel;
+        const serverQueue = client.queues.get(interaction.guild.id);
+        const voiceChannel = interaction.member.voice.channel;
 
-        const botPermissionsFor = message.channel.permissionsFor(message.guild.me);
-        if (!botPermissionsFor.has(Permissions.FLAGS.USE_EXTERNAL_EMOJIS)) return interaction.reply(client.emotes.permissionError + " **I do not have permission to Use External Emojis in** " + "`" + message.channel.name + "`");
+        const botPermissionsFor = interaction.channel.permissionsFor(interaction.guild.me);
+        if (!botPermissionsFor.has(Permissions.FLAGS.USE_EXTERNAL_EMOJIS)) return interaction.reply(client.emotes.permissionError + " **I do not have permission to Use External Emojis in** " + "`" + interaction.channel.name + "`");
 
         if (!voiceChannel) return interaction.reply(client.emotes.error + " **You have to be in a voice channel to use this command**");
 
-        if (!message.guild.me.voice.channel) return interaction.reply(client.emotes.error + " **I am not connected to a voice channel.** Type " + "`" + client.config.app.prefix + "join" + "`" + " to get me in one");
+        if (!interaction.guild.me.voice.channel) return interaction.reply(client.emotes.error + " **I am not connected to a voice channel.** Type " + "`" + client.config.app.prefix + "join" + "`" + " to get me in one");
 
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return interaction.reply(client.emotes.error + " **You need to be in the same voice channel as Bass to use this command**");
+        if (interaction.guild.me.voice.channel && interaction.member.voice.channel.id !== interaction.guild.me.voice.channel.id) return interaction.reply(client.emotes.error + " **You need to be in the same voice channel as Bass to use this command**");
 
         if (!serverQueue.tracks.length) return interaction.reply(client.emotes.error + " **Nothing playing in this server**, let's get this party started! :tada:");
 
