59cbbb1d98269cb855ffc457428774e97161581a
Logan <75645441+logan354@users.noreply.github.com>
2025-07-10 00:26:31 +1200

    Update ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -21,57 +21,5 @@ export default {
             ],
             status: "online"
         });
-
-        const textChannel = bot.channels.cache.get("886402406500429864") as TextChannel;
-        const voiceChannel = bot.channels.cache.get("1108737889434599575") as VoiceChannel;
-
-        const voiceConnection = joinVoiceChannel({
-            channelId: voiceChannel.id,
-            guildId: voiceChannel.guild.id,
-            adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-        });
-
-        const input = "https://akamd1.jw-cdn.org/sg2/p/a3deaf/3/o/sjjm_E_153.mp3"
-
-        // FFMPEG
-        let ffmpegArguments = [
-            "-reconnect", "1",
-            "-reconnect_streamed", "1",
-            "-reconnect_delay_max", "5",
-            "-i", input,
-            "-analyzeduration", "0",
-            "-loglevel", "0",
-            "-f", "opus",
-            "-ar", "48000",
-            "-ac", "2",
-        ];
-
-        const stream = new FFmpeg({
-            args: ffmpegArguments
-        });
-
-        const audioResource = createAudioResource(stream);
-
-        const audioPlayer = createAudioPlayer();
-
-        voiceConnection.subscribe(audioPlayer);
-
-        audioPlayer.play(audioResource);
-
-        const data = await youtubeDl.exec(
-            input,
-            {
-                dumpSingleJson: true,
-                noCheckCertificates: true,
-                noWarnings: true,
-                preferFreeFormats: true,
-                skipDownload: true,
-                extractAudio: true,
-            }
-        );
-
-        const dataJSON = JSON.parse(data.stdout);
-
-        console.log(dataJSON)
     }
 } as Event;
