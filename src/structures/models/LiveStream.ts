d5b11fe7c955cf5fb76762f180580526e9aa9af5
Logan <75645441+logan354@users.noreply.github.com>
2025-12-10 00:19:20 +1300

    feat: add live stream model (#10)

---
+++ src/structures/models/LiveStream.ts
@@ -0,0 +1,40 @@
+import { User } from "discord.js";
+
+import { QueueableAudioMedia } from "../AudioMedia";
+import { AudioMediaSource, QueueableAudioMediaType } from "../../utils/constants";
+
+export interface Artist {
+    name: string;
+    url?: string;
+    imageURL?: string;
+}
+
+export default class LiveStream extends QueueableAudioMedia {
+    url: string;
+
+    title: string;
+
+    artists: Artist[];
+
+    imageURL: string | null;
+
+    constructor(
+        source: AudioMediaSource,
+        requester: User | null,
+        url: string,
+        title: string,
+        artists: Artist[],
+        imageURL: string | null,
+    ) {
+        super(
+            QueueableAudioMediaType.LIVE_STREAM,
+            source,
+            requester
+        );
+
+        this.url = url;
+        this.title = title;
+        this.artists = artists;
+        this.imageURL = imageURL;
+    }
+}
