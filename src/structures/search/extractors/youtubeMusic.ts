3fd009cf5582f565775dd1d97104cbe946783760
Logan <75645441+logan354@users.noreply.github.com>
2025-12-29 13:39:19 +1300

    feat: update extractors for youtube-dl-exec

--- src/structures/search/extractors/youtubeMusic.ts
+++ src/structures/search/extractors/youtubeMusic.ts
@@ -1,7 +1,24 @@
 import { User } from "discord.js";
 
-import { searchYouTubeURL } from "./youtube";
+import { searchYouTube, searchYouTubeURL } from "./youtube";
 import SearchResult from "../SearchResult";
+import { AudioMediaType } from "../../../utils/constants";
+
+/**
+ * Searches a query on YouTube Music.
+ * Defaults type: TRACK, count: 1, requester: null.
+ * @param query 
+ * @param options 
+ * @async
+ * @returns
+ */
+export async function searchYouTubeMusic(
+    query: string,
+    options?: { type?: AudioMediaType, count?: number, requester?: User | null }
+): Promise<SearchResult> {
+    // Convert to standard youtube until proper extractor support
+    return await searchYouTube(query, options);
+}
 
 /**
  * Searches a YouTube Music url.
